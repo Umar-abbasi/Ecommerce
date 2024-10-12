577f1758f13b06bf9c7adc4da94c24dc70eaae4b
Umar Abbasi <umarabbasi11811@gmail.com>
2024-10-12 13:13:06 +0500

    day:6 part:1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Script for navigation bar
+const bar = document.querySelector("#bar");
+const nav = document.querySelector("#navbar");
+const close = document.querySelector("#close");
+if(bar){
+    bar.addEventListener("click",()=>{
+    nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener("click",()=>{
+    nav.classList.remove('active');
+    })
+}
